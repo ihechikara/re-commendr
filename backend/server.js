1e21b586a84059014dbd01d780e34944933ee5ff
ihechikara <ihechikara.dev@gmail.com>
2023-11-06 14:06:38 +0100

    feat: setup project structure
    - create controller, models, routes folders
    - install express and dotenv
    - setup server for local development

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+require("dotenv").config()
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port ${process.env.PORT}`)
+})
